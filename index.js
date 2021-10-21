1f157d929503ac628f5e98c6a6ea59ca26546fb8
GedasBu <86909912+GedasBu@users.noreply.github.com>
2021-10-21 19:19:20 +0300

    server  skeletas

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('run')
+const server = require('./lib/server.js')
+
+
+const app = {};
+
+app.init = () =>{
+// pasiruosti pradinius folderius
+// pasiruosti failus
+// prisijungti pie db
+server.init();
+
+};
+
+app.init();
+
+module.exports = app;
